4be73f70c0035226287308a408064ea0174170f9
Replit user <>
2023-06-06 14:05:47 +0000

    session 2 work

--- session_02/exercises_2.py
+++ session_02/exercises_2.py
@@ -1,5 +1,7 @@
 # Session 2 Exercises
-
+length = len("LaanTuring")
+middle_letter = "alanTuring"[int(length/2)].lower()
+print(middle_letter)
 ## Section A
 # 1. Create two variables that hold the width and height of a rectangle, work out and store the area in a third variable. 
 # Print out the string: `Rectangle of width <x> and height <y> has an area of <area>`.
@@ -15,11 +17,11 @@ print(len("Python"))
 print("Python"[0] + "Python"[2])
 
 # 4. Ask the user to enter their name, and print out `Hello, <name>`.
-name = input("what is your name?")
+name = input(str("what is your name?"))
 print("Hello, " + name)
 
 # 5. Ask the user to enter their age, tell them how old they will be in 15 years time.
-age = int(input("What is your age?"))
+age = input(int("What is your age?"))
 print("In 15 years time you will be " + str(int(age) + 15))
 
 # 6. Combine the two input statements above and print out the message `Hello, <name>, you are currently <age> years old. 
@@ -27,23 +29,27 @@ print("In 15 years time you will be " + str(int(age) + 15))
 print("Hello, "+name+", you are currently "+str(age)+" years old. In 15 years time you will be "+str(age+15))
 
 # 7. Ask the user to enter their hometown, print it out in uppercase letters.
-hometown= input("What is you're hometown?")
+hometown= input(str("What is you're hometown?"))
 print(hometown.upper())
 
 # 8. Ask the user to enter their favourite colour and find out the length of the colour they input.
-fav_colour = input("What is your favourite colour?")
+fav_colour = input(str("What is your favourite colour?"))
 print(len(fav_colour))
 
 # 9. Ask the user to enter the weather and the month. Print out the string, `It is <month> and it is <weather> today`.
-month = input("What is the month?")
-weather = input("What is the weather like today?")
+month = input(str("What is the month? "))
+weather = input(int("What temperature is it today? "))
 print("It is " +month+" and the weather is "+weather)
 
 # 10. Ask the user to enter 5 different temperatures and the month. Work out the average temperature and print out the string: 
 #   `It is <month> and the average temperature is <average_temperature> degrees celsius`.
-month = input("What is the month?")
-temp = int(input("What is temperature is it for the next 5 days?"))
-avg_temp = sum(temp)/5
+month = input(str("What is the month? "))
+temp1 = input(int("What is temperature is it for the next 5 days? Day 1: "))
+temp2 = input(int("What is temperature is it for the next 5 days? Day 2: "))
+temp3 = input(int("What is temperature is it for the next 5 days? Day 3: "))
+temp4 = input(int("What is temperature is it for the next 5 days? Day 4: "))
+temp5 = input(int("What is temperature is it for the next 5 days? Day 5: "))
+avg_temp = (temp1+temp2_temp3+temp4+temp5)/5
 print("It is " +month+" and the average temperature is "+str(avg_temp))
 
 # 11. Print out the above sentence but make the month upper case.
@@ -51,13 +57,13 @@ print("It is " +month.upper()+" and the average temperature is "+str(avg_temp))
 
 # 12. Create a variable that holds your favourite animals and print it out. 
 #    Make sure the animals are all on different lines and tabbed.
-fav_animals = input("What are your favourite animals?")
+fav_animals = input("What are your favourite animals? ")
 print("Your favourite animals are" + fav_animals)
 
 # 13. Ask the user to enter their name as well as a number. 
 #    Print out the uppercase character at that position in the string.
-name = input("What is your name?")
-fav_number = int(input("What is your favourite whole number?"))
+name = input("What is your name?" )
+fav_number = int(input("What is your favourite whole number? "))
 print(name[fav_number])
 
 # 14. Slice the string with steps of 2, excluding the first and last characters of the string "WelcometoPython".
